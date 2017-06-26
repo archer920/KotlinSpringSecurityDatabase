e5eb541616eedf398b6723dd5757575599f84393
Patrick Luck <stonesoup@Patricks-MacBook-Pro.local>
2017-06-26 19:40:28 -0400

    Spring Security with a custom login page

--- src/main/kotlin/com/stonesoupprogramming/KotlinSpringSecurityIntroductionApplication.kt
+++ src/main/kotlin/com/stonesoupprogramming/KotlinSpringSecurityIntroductionApplication.kt
@@ -1,17 +1,26 @@
 package com.stonesoupprogramming
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcOperations
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import javax.sql.DataSource
 
 @SpringBootApplication
 class KotlinSpringSecurityIntroductionApplication
@@ -20,34 +29,55 @@ fun main(args: Array<String>) {
     SpringApplication.run(KotlinSpringSecurityIntroductionApplication::class.java, *args)
 }
 
-@Configuration //Makr this as a configuration class
+@Configuration
+class DataConfig {
+
+    @Bean(name = arrayOf("dataSource"))
+    fun dataSource() : DataSource {
+        //This will create a new embedded database and run the schema.sql script
+        return EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("schema.sql")
+                .build()
+    }
+
+    @Bean
+    fun jdbcTemplate(@Qualifier("dataSource") dataSource: DataSource) : JdbcOperations {
+        return JdbcTemplate(dataSource)
+    }
+}
+
+@Configuration //Make this as a configuration class
 @EnableWebSecurity //Turn on Web Security
-class SecurityWebInitializer : WebSecurityConfigurerAdapter(){
+class SecurityWebInitializer(
+        //Inject our datasource into this class for the AuthenticationManagerBuilder
+        @Autowired @Qualifier("dataSource") val dataSource: DataSource)
+    : WebSecurityConfigurerAdapter(){
+
     override fun configure(http: HttpSecurity) {
-        //This tells Spring Security to authorize all requests
-        //We use formLogin and httpBasic
         http
-                .authorizeRequests()
-                    .anyRequest()
-                    .authenticated()
-                .and()
                     .formLogin()
                 .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .rememberMe()
+                        .tokenRepository(JdbcTokenRepositoryImpl())
+                            .tokenValiditySeconds(2419200)
+                                .key("BurgerBob")
+                .and()
                     .httpBasic()
+                .and()
+                    .authorizeRequests()
+                        .antMatchers("/").authenticated()
+                        .anyRequest().permitAll()
     }
 
     override fun configure(auth: AuthenticationManagerBuilder) {
-        //This code sets up a user store in memory. This is
-        //useful for debugging and development
-        auth
-                .inMemoryAuthentication()
-                    .withUser("bob")
-                    .password("belcher")
-                    .roles("USER")
-                .and()
-                    .withUser("admin")
-                    .password("admin")
-                    .roles("USER", "ADMIN")
+        //As long as our database schema conforms to the default queries
+        //we can use jdbcAuthentication and pass in our data source
+        //Spring will do the rest of the work for us
+        auth.jdbcAuthentication().dataSource(dataSource)
     }
 }
 
